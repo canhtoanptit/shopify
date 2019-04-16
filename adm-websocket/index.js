860bfd1909f2719acb61976e180726491f671efe
ToanNC <toan.nguyen.canh@ntq-solution.com.vn>
2019-04-16 22:19:16 +0700

    up

--- adm-websocket/index.js
+++ adm-websocket/index.js
@@ -1,12 +1,21 @@
 const express = require('express')();
+const Shopify = require('shopify-api-node');
 const bodyParser = require('body-parser');
+const server = require('./src/service/socketio.service')(express);
 express.use(bodyParser.json());
 
-express.get('/', function (req, res) {
-  res.send('hello user!')
+const shopify = new Shopify({
+  shopName: 'vuzila',
+  apiKey: '',
+  password: ''
 });
 
-const server = require('./src/service/socketio.service')(express);
+express.get('/', function (req, res) {
+  shopify.order.list()
+    .then(res => console.log(res))
+    .catch(e => console.log(e));
+  res.send('hello user!')
+});
 
 server.listen(3000, function () {
   console.log("server listen at port 3000")
