ae05cfaf1fa4ddceb6f98ff4e0bda7ea8d1fa8fd
ToanNC <toan.nguyen.canh@ntq-solution.com.vn>
2019-04-19 00:10:50 +0700

    update

--- adm-websocket/index.js
+++ adm-websocket/index.js
@@ -1,10 +1,12 @@
 const express = require('express')();
 const bodyParser = require('body-parser');
 const server = require('./src/service/socketio.service')(express);
+const shopifyService = require('./src/service/shopify.service');
 
 express.use(bodyParser.json());
 
 express.get('/', function (req, res) {
+  shopifyService.getListOrder();
   res.send('hello user!')
 });
 
--- adm-websocket/src/service/shopify.service.js
+++ adm-websocket/src/service/shopify.service.js
@@ -1,7 +1,22 @@
 const Shopify = require('shopify-api-node');
+const moment = require('moment-timezone');
 
-// const shopify = new Shopify({
-//     shopName: 'vuzila',
-//     apiKey: '',
-//     password: ''
-// });
+const shopify = new Shopify({
+  shopName: 'vuzila',
+  apiKey: '',
+  password: ''
+});
+
+const getListOrder = function () {
+  const startOfDay = moment.tz('America/Juneau').startOf('day').format();
+  shopify.order.list({limit: 1, fields: 'name,created_at,total_price,line_items', created_at_min: startOfDay})
+    .then(res => console.log('response: ', res))
+    .catch(error => console.log('error ', error));
+  // shopify.order.count({created_at_min : startOfDay})
+  //   .then(res => console.log('response: ', res))
+  //   .catch(error => console.log('error ', error));
+};
+
+module.exports = {
+  getListOrder
+};
