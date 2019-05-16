123af36aead37643e236c234aa5c5290b06333cc
ToanNC <toan.nguyen.canh@ntq-solution.com.vn>
2019-05-17 04:07:46 +0700

    up

--- adm-websocket/index.js
+++ adm-websocket/index.js
@@ -2,65 +2,14 @@ const express = require('express')();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const server = require('./src/service/socketio.service')(express);
-const shopifyService = require('./src/service/shopify.service');
-const validation = require('./src/util/validation');
+const orderRouter = require('./src/routes/orders.route');
+const productRoute = require('./src/routes/product.route');
 
 express.use(bodyParser.json());
 express.use(cors());
 
-express.get('/api/orders', async function (req, res) {
-  try {
-    const token = req.headers.authorization.split(" ")[1];
-    validation.verifyToken(token, function (payload, err) {
-      if (err) {
-        console.log('error: ', err);
-        res.status(401);
-        res.send('invalid token')
-      }
-      if (payload) {
-        shopifyService.getListOrder()
-          .then(result => res.send(result))
-          .catch(err => {
-            console.log('error: ', err);
-            res.status(500);
-            res.send('internal error')
-          })
-      }
-    });
-  } catch (e) {
-    console.log('error: ', e);
-    res.status(500);
-    res.send('internal error')
-  }
-});
-
-express.post('/api/product', async function (req, res) {
-  try {
-    const token = req.headers.authorization.split(" ")[1];
-    validation.verifyToken(token, function (payload, err) {
-      if (err) {
-        console.log('error: ', err);
-        res.status(401);
-        res.send('invalid token')
-      }
-      if (payload) {
-        shopifyService.getAllProduct()
-          .then(() => {
-            res.send('ok')
-          })
-          .catch(err => {
-            console.log('error: ', err);
-            res.status(500);
-            res.send('internal error')
-          })
-      }
-    });
-  } catch (e) {
-    console.log('error: ', e);
-    res.status(500);
-    res.send('internal error')
-  }
-});
+express.use('/api/orders', orderRouter);
+express.use('/api/products', productRoute);
 
 server.listen(3001, function () {
   console.log("server listen at port 3001")
