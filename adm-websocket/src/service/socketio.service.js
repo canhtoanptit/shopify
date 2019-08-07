dd294072671dc5b69cf2b3ae6da540919fc2dc27
nguyen canh toan <v.toannc@vinid.net>
2019-08-07 08:11:38 +0700

    add paypal sdk

--- adm-websocket/src/service/socketio.service.js
+++ adm-websocket/src/service/socketio.service.js
@@ -1,13 +1,13 @@
 const CronJob = require('cron').CronJob;
 const shopifyService = require('./shopify.service');
 
-let ordersScheduler = function (io) {
-  new CronJob('*/10 * * * *', async function () {
-    console.log('you will see this message every 10 minute');
-    const data = await shopifyService.getListOrder();
-    io.to('1').emit('update_order', {data: data})
-  }, null, true, 'America/Los_Angeles');
-};
+// let ordersScheduler = function (io) {
+//   new CronJob('*/10 * * * *', async function () {
+//     console.log('you will see this message every 10 minute');
+//     const data = await shopifyService.getListOrder();
+//     io.to('1').emit('update_order', {data: data})
+//   }, null, true, 'America/Los_Angeles');
+// };
 
 let ioEvents = function (io) {
   io.on('connection', function (socket) {
