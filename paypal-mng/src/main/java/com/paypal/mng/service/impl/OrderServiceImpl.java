d9d55324eb5f11f3ad0df19b8ad9209d138a6738
ToanNC <toan.nguyen.canh@ntq-solution.com.vn>
2019-08-25 17:55:51 +0700

    up

--- paypal-mng/src/main/java/com/paypal/mng/service/impl/OrderServiceImpl.java
+++ paypal-mng/src/main/java/com/paypal/mng/service/impl/OrderServiceImpl.java
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -100,7 +101,7 @@ public class OrderServiceImpl implements OrderService {
      */
     @Override
     public Page<Order> findByOrderNameContain(String orderName) {
-        return orderRepository.findByOrderNameContaining(orderName);
+        return orderRepository.findByOrderNameContaining(orderName, PageRequest.of(0, 20));
     }
 
     /**
